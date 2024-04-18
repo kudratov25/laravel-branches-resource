23f91f56b56419c08c34bf305b599b747c60f8fe
kudratov25 <qudratov25@mail.ru>
2024-04-18 17:32:12 +0500

    vonage config default false

--- config/vonage.php
+++ config/vonage.php
@@ -2,7 +2,7 @@
 
 return [
     'key' => env('VONAGE_KEY'),
-    'send' => env('VONAGE_SEND', true),
+    'send' => env('VONAGE_SEND', false),
     'secret' => env('VONAGE_SECRET'),
     'attempts' => env('VONAGE_ATTEMPTS'),
     'sms_from' => env('VONAGE_SMS_FROM'),
